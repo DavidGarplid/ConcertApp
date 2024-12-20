356c89354310f5d5faef6b8377ba02d5ad1635fa
MilMax1 <167527438+MilMax1@users.noreply.github.com>
2024-12-20 11:56:29 +0100

    Added Entities

---
+++ ConcertApp.Data/Entity/Concert.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertApp.Data.Entity
+{
+    internal class Concert
+    {
+    }
+}
---
+++ ConcertApp.Data/Entity/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertApp.Data.Entity
+{
+    internal class User
+    {
+        string id;
+        string name;
+        string email;
+        string password;
+    }
+}
